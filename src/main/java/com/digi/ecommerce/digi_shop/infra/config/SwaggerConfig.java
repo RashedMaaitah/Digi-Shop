a1309513fdf11ff734e82e62a2dceeabfd589daf
Rashed Maaitah <bahran568@gmail.com>
2025-01-29 22:50:26 +0300

    chore: add swagger config

---
+++ src/main/java/com/digi/ecommerce/digi_shop/infra/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.digi.ecommerce.digi_shop.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP;
+
+@Slf4j
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        System.out.println(appVersion);
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("REST API for Flight Advisor Service.")
+//                        .version(this.appVersion)
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://springdoc.org")));
+    }
+
+}
