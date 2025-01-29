f4ce5300d65a4cfab4dc46240283f8fe2badcfa9
Rashed Maaitah <bahran568@gmail.com>
2025-01-29 22:42:15 +0300

    chore: initial project setup

---
+++ src/main/java/com/digi/ecommerce/digi_shop/DigiShopApplication.java
@@ -0,0 +1,13 @@
+package com.digi.ecommerce.digi_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digi/ecommerce/digi_shop/DigiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digi.ecommerce.digi_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
